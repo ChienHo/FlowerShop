0c6c69bcd7f056d36d4b34d95e1978c636fe9933
ChienHo <Chienhos2@gmail.com>
2019-04-19 17:07:13 +0700

    add seaching

--- FA.FlowerShop/FA.FlowerShop.Presentation/Controllers/FlowerController.cs
+++ FA.FlowerShop/FA.FlowerShop.Presentation/Controllers/FlowerController.cs
@@ -11,16 +11,29 @@ namespace FA.FlowerShop.Presentation.Controllers
     public class FlowerController : Controller
     {
         private readonly IGenericRepository<Flower> _genericRepository;
+        private readonly IGenericRepository<Category> _caterepository;
 
-        public FlowerController(IGenericRepository<Flower> genericRepository)
+        public FlowerController(IGenericRepository<Flower> genericRepository, IGenericRepository<Category> caterepository)
         {
             _genericRepository = genericRepository;
+            _caterepository = caterepository;
         }
 
         // GET: Flower
-        public ActionResult Index()
+        public ActionResult Index(int? category, string flowerName, int? price1, int? price2)
         {
-            return View(_genericRepository.GetAll());
+            var listflower = _genericRepository.GetAll();
+
+            if (category != null && !String.IsNullOrEmpty(flowerName))
+            {
+                listflower = _genericRepository.GetAll().Where(p => p.CategoryId == category.Value && p.FlowerName.Contains(flowerName) && p.Price > price1 && p.Price < price2);
+            }
+            if (category == null && !String.IsNullOrEmpty(flowerName))
+            {
+                listflower = _genericRepository.GetAll().Where(p => p.FlowerName.Contains(flowerName) && p.Price > price1 && p.Price < price2);
+            }
+            ViewBag.Category = _caterepository.GetAll();
+            return View(listflower);
         }
         public ActionResult Details(int? id)
         {
